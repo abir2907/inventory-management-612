48797dfcc0823e440795ab40482282c81f1aed92
Abir Chodha <abirchodha1@gmail.com>
2025-08-05 19:08:27 +0530

    Fix deployment issues

--- backend/server.js
+++ backend/server.js
@@ -114,9 +114,4 @@ app.use("*", (req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
